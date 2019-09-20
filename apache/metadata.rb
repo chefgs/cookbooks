7bd71b629442361fae1a5137c6dfadade08b5f44
Saravanan G <g.gsaravanan@gmail.com>
2019-09-20 17:31:09 +0530

    Update metadata.rb

--- apache/metadata.rb
+++ apache/metadata.rb
@@ -1,6 +1,6 @@
 name 'apache'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Saravanan G'
+maintainer_email 'g.gsaravanan@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
