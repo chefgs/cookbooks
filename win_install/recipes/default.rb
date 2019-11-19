35a2a6a83d6caa185c3680447aef40fa844b3963
Saravanan G <g.gsaravanan@gmail.com>
2019-11-19 20:11:33 +0000

    registry recipe added to disable IE ES

--- win_install/recipes/default.rb
+++ win_install/recipes/default.rb
@@ -10,5 +10,16 @@ unless platform?('windows')
   return 0
 end
 
-node.default['sql_server']['accept_eula'] = 'true'
-include_recipe 'sql_server::client'
+
+# Disable Windows Server IE Enhanced Security Feature
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}' do
+  values [{
+    name: 'IsInstalled',
+    type: :dword,
+    data: 0
+  }]
+  action :create
+end
+
+#node.default['sql_server']['accept_eula'] = 'true'
+#include_recipe 'sql_server::client'
